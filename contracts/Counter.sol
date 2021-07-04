b64e8ff137ec9bc6ab9f480ea952857d91333e6b
Bharat Kulkarni <kulkarnibharat018@gmail.com>
2021-07-04 18:36:11 +0530

    first commit

---
+++ contracts/Counter.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Counter{
+    
+    uint public count = 0;
+    
+    function IncrementCount() public {
+        count++;
+    }
+    
+}
---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.4 <0.9.0;
+
+contract HelloWorld{
+    
+    
+    function helloWorld() public pure returns (string memory) {
+        return "Hello World";
+    }
+    
+}
